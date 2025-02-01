dbf1c97be4f4b4bfc468e420c47fb4a5a16e2787
DiansSopandi <dian.sopandi@gmail.com>
2025-02-01 12:18:49 +0700

    delete index.js and scraper.js

--- models/Pokemon.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PokemonSchema = new mongoose.Schema({
-  name: String,
-  type: [String],
-  imageUrl: String
-});
-
-module.exports = mongoose.model('Pokemon', PokemonSchema);
